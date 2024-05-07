321d743af8e235a9307430aba17b8f05799e440e
Andrei Kislichenko <android.2net@gmail.com>
2024-05-06 22:58:39 -0400

    Update text model

--- lib/relaton_ecma/xml_parser.rb
+++ lib/relaton_ecma/xml_parser.rb
@@ -1,13 +1,11 @@
 module RelatonEcma
-  class XMLParser < RelatonBib::XMLParser
-    class << self
-      private
-
-      # @param item_hash [Hash]
-      # @return [RelatonEcma::BibliographicItem]
-      def bib_item(item_hash)
-        BibliographicItem.new(**item_hash)
-      end
+  module XMLParser
+    include RelatonBib::Parser::XML
+    extend self
+    # @param item_hash [Hash]
+    # @return [RelatonEcma::BibliographicItem]
+    def bib_item(item_hash)
+      BibliographicItem.new(**item_hash)
     end
   end
 end
